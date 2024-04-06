6e72bf68437d32a9989acad102b4faed82b2fec3
Danny Cao <dannycao1997@yahoo.com>
2024-04-05 22:52:43 -0400

    add constructors,getters/setters, and test for fishing rod

--- src/main/java/models/FishingRod.java
+++ src/main/java/models/FishingRod.java
@@ -8,4 +8,62 @@ public class FishingRod {
     private int size;
     private int qty;
     private float price;
+
+
+    public FishingRod(){
+    }
+    public FishingRod(int id, String name, String brand, String sport, int qty, float price) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.sport = sport;
+        this.qty = qty;
+        this.price = price;
+    }
+
+    public int getID() {
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
+
--- src/test/java/models/FishingRodTest.java
+++ src/test/java/models/FishingRodTest.java
@@ -1,4 +1,44 @@
 package models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class FishingRodTest {
+    private FishingRod fishingRod;
+    @Test
+    public void setNameTest(){
+        //given (1)
+        String expected = "5000 Series Titanium Shark Reel";
+
+        //when (2)
+        fishingRod = new FishingRod();
+        fishingRod.setName(expected);
+
+        //then (3)
+        Assertions.assertEquals(expected, fishingRod.getName());
+    }
+
+    @Test // (1)
+    public void constructorTest(){
+
+        // (2)
+        int expectedID = 99;
+        String expectedName  = "Carbon Fiber Surf Rod";
+        String expectedBrand = "Danny's Fishing Company";
+        String expectedSport = "Fishing";
+        int expectedQty = 5000;
+        float expectedPrice = 150.00f;
+
+        // (3)
+        FishingRod fishingRod = new FishingRod(expectedID, expectedName, expectedBrand, expectedSport, expectedQty, expectedPrice);
+
+        // (4)
+        Assertions.assertEquals(expectedID, fishingRod.getID());
+        Assertions.assertEquals(expectedName, fishingRod.getName());
+        Assertions.assertEquals(expectedBrand, fishingRod.getBrand());
+        Assertions.assertEquals(expectedSport, fishingRod.getSport());
+        Assertions.assertEquals(expectedQty, fishingRod.getQty());
+        Assertions.assertEquals(expectedPrice, fishingRod.getPrice());
+
+    }
 }
