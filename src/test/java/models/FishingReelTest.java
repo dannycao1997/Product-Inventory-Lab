7e6ea84bbd993792c146ae0becbb593a7ec079d8
Danny Cao <dannycao1997@yahoo.com>
2024-04-05 22:31:56 -0400

    add constructors

--- src/test/java/models/FishingReelTest.java
+++ src/test/java/models/FishingReelTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FishingReelTest {
-    FishingReel fishingReel;
+    private FishingReel fishingReel;
     @Test
     public void setNameTest(){
         //given (1)
@@ -16,5 +16,29 @@ public class FishingReelTest {
 
         //then (3)
         Assertions.assertEquals(expected, fishingReel.getName());
+    }
+
+    @Test // (1)
+    public void constructorTest(){
+
+        // (2)
+        int expectedID = 26;
+        String expectedName  = "5000 Series Titanium Shark Reel";
+        String expectedBrand = "Danny's Fishing Company";
+        String expectedSport = "Fishing";
+        int expectedQty = 1000;
+        float expectedPrice = 200.00f;
+
+        // (3)
+        FishingReel fishingReel = new FishingReel(expectedID, expectedName, expectedBrand, expectedSport, expectedQty, expectedPrice);
+
+        // (4)
+        Assertions.assertEquals(expectedID, fishingReel.getID());
+        Assertions.assertEquals(expectedName, fishingReel.getName());
+        Assertions.assertEquals(expectedBrand, fishingReel.getBrand());
+        Assertions.assertEquals(expectedSport, fishingReel.getSport());
+        Assertions.assertEquals(expectedQty, fishingReel.getQty());
+        Assertions.assertEquals(expectedPrice, fishingReel.getPrice());
+
     }
 }
