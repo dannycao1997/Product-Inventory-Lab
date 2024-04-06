a7252ccd6fbf677d97a6d7026f2d5ecf6d847f83
Danny Cao <dannycao1997@yahoo.com>
2024-04-06 00:15:34 -0400

    :O

--- src/main/java/services/FishingReelService.java
+++ src/main/java/services/FishingReelService.java
@@ -8,4 +8,17 @@ import java.util.ArrayList;
 public class FishingReelService {
     private static int nextID = 1; // (1)
     private ArrayList<FishingReel> inventory = new ArrayList<>(); // (2)
+
+    // (1)
+    public FishingReel create(String name, String brand, String sport, int quantity, float price) {
+
+        // (2)
+        FishingRod createdFishingReel = new FishingReel(nextId++, name, brand, sport, quantity, price);
+
+        // (3)
+        inventory.add(createdFishingReel);
+
+        // (4)
+        return createdFishingReel;
+    }
 }
--- src/main/java/services/FishingRodService.java
+++ src/main/java/services/FishingRodService.java
@@ -7,4 +7,19 @@ import java.util.ArrayList;
 public class FishingRodService {
     private static int nextID = 1; // (1)
     private ArrayList<FishingRod> inventory = new ArrayList<>(); // (2)
+
+    // (1)
+    public FishingRod create(String name, String brand, String sport, int quantity, float price) {
+
+        // (2)
+        FishingRod createdFishingRod = new FishingRod(nextId++, name, brand, sport, quantity, price);
+
+        // (3)
+        inventory.add(createdFishingRod);
+
+        // (4)
+        return createdFishingRod;
+    }
+
+
 }
